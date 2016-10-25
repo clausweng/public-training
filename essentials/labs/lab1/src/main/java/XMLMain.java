b4954da2541409832171c7591e757a3e710cfef2
Rahul Gupta <rahul@hazelcast.com>
2016-10-25 14:05:37 +0800

    Update XMLMain.java

--- essentials/labs/lab1/src/main/java/XMLMain.java
+++ essentials/labs/lab1/src/main/java/XMLMain.java
@@ -2,17 +2,8 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
-<<<<<<< HEAD
 public class XMLMain {
     public static void main(String[] args) {
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-=======
-// https://github.com/hazelcast/hazelcast/issues/9081
-
-public class XMLMain {
-    public static void main(String[] args) {
-    	HazelcastInstance hz = Hazelcast.newHazelcastInstance();
->>>>>>> upstream/master
-        HazelcastInstance client = HazelcastClient.newHazelcastClient();
     }
 }
